6d5fe6eb9d381d71681099ca425294005f34568d
Digtyarenk0 <uhmeplay811@gmail.com>
2024-03-18 14:55:50 +0100

    add . files

--- src/metadata.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-export default async () => {
-    const t = {};
-    return { "@nestjs/swagger/plugin": { "models": [], "controllers": [[import("./app/init/init.controller"), { "InitController": { "getHello": { type: String } } }], [import("./app/init/controller/init.controller"), { "InitController": { "getHello": { type: String } } }]] } };
-};
